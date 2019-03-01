0f1e4289e98de9a34b791b35f98fb1d0c6823757
huangwenming <1205597228@qq.com>
2019-03-01 14:50:23 +0800

    error-report发起jsonp请求暂缓2s执行，同时将stack内容进行encode处理

--- error-report/examples/client/test-error/bundle.js
+++ error-report/examples/client/test-error/bundle.js
@@ -265,6 +265,10 @@ window.jsonpcb = {};
         url = url + (url.indexOf('?') > -1 ? '&' : '?') + cbParamType + '=window.jsonpcb.' + cbn;
         data.ctime = new Date().getTime();
         for (let key in data) {
+            // 需要对stack中的内容进行encodeURIComponent处理，避免由于stack中的特殊字符导致http请求被blocked
+            if (key === 'stack' && data[key]) {
+                data[key] = encodeURIComponent(data[key]);
+            }
             if (typeof data[key] === 'object') {
                 url += '&' + key + '=' + JSON.stringify(data[key]);
             }
@@ -273,20 +277,21 @@ window.jsonpcb = {};
             }
         }
         let script = document.createElement('script');
-        window.jsonpcb[cbn] = function (response) {
+        window.jsonpcb[cbn] = (response)=> {
             script.parentNode.removeChild(script);
             successCb && successCb(response);
             window.jsonpcb[cbn] = null;
         };
-        script.onerror = function () {
+        script.onerror = ()=> {
             script.parentNode.removeChild(script);
             failCb && failCb();
-            // util.toast('请求失败，请重试');
             window.jsonpcb[cbn] = null;
         };
-        script.src = url;
+        // console.log(url);
         script.type = 'text/javascript';
-        document.body.appendChild(script);
+        script.src = url;
+        // 请求2s延缓发出，尽量在页面加载的时候，不占用资源
+        setTimeout(()=>{document.body.appendChild(script);}, 2000);
     }
 });
 
--- error-report/libs/util.js
+++ error-report/libs/util.js
@@ -16,6 +16,10 @@ export default {
         url = url + (url.indexOf('?') > -1 ? '&' : '?') + cbParamType + '=window.jsonpcb.' + cbn;
         data.ctime = new Date().getTime();
         for (let key in data) {
+            // 需要对stack中的内容进行encodeURIComponent处理，避免由于stack中的特殊字符导致http请求被blocked
+            if (key === 'stack' && data[key]) {
+                data[key] = encodeURIComponent(data[key]);
+            }
             if (typeof data[key] === 'object') {
                 url += '&' + key + '=' + JSON.stringify(data[key]);
             }
@@ -24,19 +28,20 @@ export default {
             }
         }
         let script = document.createElement('script');
-        window.jsonpcb[cbn] = function (response) {
+        window.jsonpcb[cbn] = (response)=> {
             script.parentNode.removeChild(script);
             successCb && successCb(response);
             window.jsonpcb[cbn] = null;
         };
-        script.onerror = function () {
+        script.onerror = ()=> {
             script.parentNode.removeChild(script);
             failCb && failCb();
-            // util.toast('请求失败，请重试');
             window.jsonpcb[cbn] = null;
         };
-        script.src = url;
+        // console.log(url);
         script.type = 'text/javascript';
-        document.body.appendChild(script);
+        script.src = url;
+        // 请求2s延缓发出，尽量在页面加载的时候，不占用资源
+        setTimeout(()=>{document.body.appendChild(script);}, 2000);
     }
 }
