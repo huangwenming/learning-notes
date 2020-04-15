efa1e94274597ca34e8489078fda86e8d9748d2c
huangwenming <1205597228@qq.com>
2020-04-15 09:13:10 +0800

    feat(designmode/smd): add module apply method

--- 100days/designModes/smd.js
+++ 100days/designModes/smd.js
@@ -59,6 +59,56 @@ F.define('string', function () {
         }
     };
 });
-var testStr = ' test trim method of string module ';
-// 打印：test trim method of string module
-console.log(F.string.trim(testStr));
+
+/**
+ * 模块的调用方法
+ *
+ * @method module
+ * @param {...(string | Object) | Array} moudleNames 依赖模块，可以是可变字符串也可以是数组
+ * @param {Function} func 执行函数
+ */
+F.module = function (moudleNames, func) {
+    var args = Array.prototype.slice.call(arguments);
+    // 获取执行函数
+    func = args.pop();
+    // 获取依赖模块数组
+    moudleNames = args[0] && args[0] instanceof Array ? args[0] : args;
+    var parent;
+    var modules = [];
+    for (var i = 0; i < moudleNames.length; i++) {
+        // 引入的是模块的路径名，则根据路径名深度遍历，获取相应的模块
+        if (typeof moudleNames[i] === 'string') {
+            parent = this;
+            var modulePaths = moudleNames[i].replace(/^F\./, '').split('.');
+            for (var j = 0; j < modulePaths.length; j++) {
+                parent = parent[modulePaths[j]];
+            }
+            modules.push(parent);
+        }
+        // 引用对象，则直接赋值
+        else if (moudleNames[i] instanceof Object) {
+            modules.push(moudleNames[i]);
+        }
+
+    }
+    // 执行回调函数，并将依赖模块作为参数传入
+    func.apply(null, modules);
+};
+
+// 调用创建的模块
+F.module(['string.trim'], function (trim) {
+    var testStr = ' test trim method of string module ';
+    // 打印：test trim method of string module
+    console.log(trim(testStr));
+});
+F.module('string.trim', function (trim) {
+    var testStr = ' test trim method of string module ';
+    // 打印：test trim method of string module
+    console.log(trim(testStr));
+});
+
+// 启发：同步模块模式是模块化开发最简单的形式，使用模块时，模块已经创建完毕，即创建即执行；
+// 同步模块模式无法处理异步加载的模块，自然不适合在浏览器环境使用；但对于node来说，文件大多存在本地，适合同步模块模式；
+
+// 参考资料
+// book：JavaScript设计模式：https://u.jd.com/Or0yQC
